79f1e0c7ac98ff22c67465a07412bda268e8237b
daisy-ua <eightnum.81@gmail.com>
2025-02-11 16:38:10 +0200

    chore: setup project

---
+++ app/src/main/java/com/daisy/journalapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.daisy.journalapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.daisy.journalapp.ui.theme.JournalAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JournalAppTheme {
+            }
+        }
+    }
+}
